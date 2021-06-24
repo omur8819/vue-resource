41ab2f89e2eb8cfef08c5db86f2cf4bfa7e1823f
omurbozdag <omur8819@gmail.com>
2021-06-24 14:52:27 +0300

    feat: added get, post, put, delete methods

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,72 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Vue-Resource</h3>
+        <div class="form-group">
+          <input type="text" class="form-control" v-model="userName">
+        </div>
+        <button class="btn btn-primary" @click="saveUser">Kaydet</button>
+        <button class="btn btn-success" @click="getUsers">Verileri Getir</button>
+        <hr>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="(user, idx) in userList" :key="idx">
+            <span>{{ user.data.userName }}</span>
+            <button class="btn btn-xs btn-danger" @click="deleteUser(user.key)">Sil</button>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    data() {
+      return {
+        userName: null,
+        userList: [],
+        resource : {}
+      }
+    },
+    methods: {
+      saveUser() {
+        // this.$http.post("users.json", {userName: this.userName})
+        //   .then((response) => {
+        //     console.log(response);
+        //   })
+        // this.$resource("users.json").save({}, { userName : this.userName })
+        this.resource.kaydet({}, { userName : this.userName });
+      },
+      getUsers() {
+        this.$resource("users.json").get()
+          .then((response) => {
+            return response.json();
+          }).then(data => {
+          for (let key in data.userList) {
+            this.userList.push(
+              {
+                key : key,
+                data : data.userList[key]
+              }
+            );
+          }
+        })
+      },
+      deleteUser(userKey){
+        // this.$http.delete("users/" + userKey + ".json")
+        //   .then(response => {
+        //     console.log(response);
+        //   })
+        this.$resource.delete("users/" + userKey + ".json");
+      }
+
+    },
+    created(){
+      const customActions = {
+        kaydet : { method : "POST", url : "users.json"}
+      };
+      this.resource = this.$resource("users.json", {}, customActions);
+    }
+  }
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from "vue-resource";
+
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://vue-resource-f89ad-default-rtdb.europe-west1.firebasedatabase.app";
+
+Vue.http.interceptors.push((request, next) => {
+  // if(request.method == "POST"){
+  //   request.method = "PUT"
+  // }
+  next(response => {
+    response.json = () => {
+      return {
+        userList : response.body
+      }
+    }
+  });
+});
 
 new Vue({
   el: '#app',
